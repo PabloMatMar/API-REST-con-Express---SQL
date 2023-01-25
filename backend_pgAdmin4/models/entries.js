a4c8eb625cec489dd61b707fbe7e5be4d1830e88
PabMatMar <pabmatmar@gmail.com>
2023-01-25 01:10:52 +0100

    add / all paths works

---
+++ backend_pgAdmin4/models/entries.js
@@ -0,0 +1,130 @@
+const { Pool } = require('pg');
+const queries = require('./queries')
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    database: 'postgres',
+    password: 'yiyomate1'
+})
+
+
+//RUTAS ENTRIES
+// GET
+const getEntriesByEmail = async (email) => {
+    let client, result;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(queries.getEntriesByEmail, [email])
+        result = data.rows
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+
+// GET
+const getAllEntries = async () => {
+    let client, result;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(queries.getAllEntries)
+        result = data.rows
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+
+// CREATE
+const createEntry = async (entry) => {
+    const { title, content, email, category } = entry;
+    let client, result;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(queries.createEntry,[title, content, email, category])
+        result = data.rowCount
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+
+// DELETE 
+const deleteEntry = async (title) => {
+    let client, result;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(queries.deleteEntry,[title])
+        result = data.rowCount
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+//UPDATE
+
+const updateEntry = async (entry, oldtitle) => {
+    const { title, content, date, category } = entry;
+    let client, result;
+    try {
+        client = await pool.connect(); // Espera a abrir conexion
+        const data = await client.query(queries.updateEntry,[title, content, date, category, oldtitle])
+        result = data.rowCount
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        client.release();
+    }
+    return result
+}
+
+
+
+
+const entries = {
+    getEntriesByEmail,
+    getAllEntries,
+    createEntry,
+    deleteEntry,
+    updateEntry
+}
+
+module.exports = entries;
+
+
+// Pruebas
+/*
+    getEntriesByEmail("birja@thebridgeschool.es")
+    .then(data=>console.log(data))
+*/
+
+/*
+getAllEntries()
+.then(data=>console.log(data))
+*/
+
+/*
+let newEntry = {
+    title: "Se acabaron las mandarinas de TB",
+    content: "Corren rumores de que papa noel tenía un saco vacio y lo llenó",
+    email: "guillermu@thebridgeschool.es",
+    category: "sucesos"
+}
+
+createEntry(newEntry)
+    .then(data => console.log(data))
+    */
+
