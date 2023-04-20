fbd39e48b218b8de512b42d656a4fb9a7d6b1174
PabMatMar <pabmatmar@gmail.com>
2023-04-20 19:16:13 +0200

    add/ ; in lines and try catch in controllers

--- backend_pgAdmin4/models/demo_pg.js
+++ backend_pgAdmin4/models/demo_pg.js
@@ -1,22 +1,22 @@
 const { Pool } = require('pg');
-const key = require('../utils/db_pgsql.js')
+const key = require('../utils/db_pgsql.js');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
     database: 'postgres',
     password: key
-})
+});
 
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error('Error acquiring client', err.stack)
+    return console.error('Error acquiring client', err.stack);
   }
   client.query('SELECT NOW()', (err, result) => {
-    release()
+    release();
     if (err) {
-      return console.error('Error executing query', err.stack)
+      return console.error('Error executing query', err.stack);
     }
-    console.log(result.rows)
-  })
-})
+    console.log(result.rows);
+  });
+});
